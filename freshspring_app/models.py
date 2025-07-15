5676b2ff75725e5f5d85f1e87e0e1e073b6d221f
lookshines <lookshinesy2k@yahoo.com>
2025-07-16 00:10:21 +0100

    more front page update

--- freshspring_app/models.py
+++ freshspring_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Services(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='services-images/', blank=True, null=True)
+    icon = models.ImageField(upload_to='services-icons/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
--- freshspring_app/views.py
+++ freshspring_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import Services
 
 # Create your views here.
 def index(request):
-    return render(request, 'freshspring_app/index.html')
+    services = Services.objects.all()
+    return render(request, 'freshspring_app/index.html', {'services': services})
 
 def services(request):
-    return render(request, 'freshspring_app/services.html')
+    services = Services.objects.all()
+    return render(request, 'freshspring_app/services.html', {'services': services})
 
 def portfolio(request):
     return render(request, 'freshspring_app/portfolio.html')
